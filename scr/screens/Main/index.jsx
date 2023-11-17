605975801a818573187c46f4567274654cd8ea8c
dudaroch <134696629+dudaroch@users.noreply.github.com>
2023-11-17 12:05:08 +0000

    commit

---
+++ scr/screens/Main/index.jsx
@@ -0,0 +1,6 @@
+import { Box, Text } from "@gluestack-ui/themed";
+
+
+export default function Main() {
+    return <Box bg="$primary100" p="$5"> <Text>Projeto GlueStack - Alterado </Text> </Box>
+}
